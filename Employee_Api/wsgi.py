f7fa56de25e4190d87950335d5e406e018227caa
viveka-n-hebbar <viveknhebbar9164@gmail.com>
2026-01-12 21:28:36 +0530

    Update wsgi.py

--- Employee_Api/wsgi.py
+++ Employee_Api/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# --- 1. Set Django settings module first ---
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Employee_Api.settings')
 
+# --- 2. Initialize Django application ---
 application = get_wsgi_application()
+
+# --- 3. Run test admin creation after Django is ready ---
+from employees.create_test_admin import run as create_admin
+create_admin()
